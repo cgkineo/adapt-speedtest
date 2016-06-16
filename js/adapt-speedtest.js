504530aaaed41db33ed33b8721b64bb647385898
Oliver Foster <oliver.foster@kineo.com>
2016-06-16 09:39:18 +0100

    update

--- js/adapt-speedtest.js
+++ js/adapt-speedtest.js
@@ -10,7 +10,7 @@ define([
         sample_age: 60000, // take lowest speed in latest timeslice (1min)
         idle_timeout: 60000, // time before idle state (1min)
         offline_threshold_kbps: 0,
-        slow_threshold_kbps: 1000,
+        slow_threshold_kbps: 2500,
     });
 
     Adapt.once('configModel:dataLoaded', function(view) {
@@ -22,6 +22,23 @@ define([
             console.log("speedtest disabled", speedtest.name, speedtest.kbps);
             return;
         }
+
+        var url = document.createElement("a");
+        url.href = window.location.href;
+        if (url.search.indexOf("speedtest=slow") > -1) {
+            speedtest.low_kbps = speedtest.kbps = 400;
+            speedtest.low_name = speedtest.name = "slow";
+            $("html").addClass("speedtest-slow").removeClass("speedtest-fast");
+            console.log("speedtest force", speedtest.name, speedtest.kbps);
+            return;
+        } else if (url.search.indexOf("speedtest=fast") > -1) {
+            speedtest.low_kbps = speedtest.kbps = 100000000;
+            speedtest.low_name = speedtest.name = "fast";
+            $("html").removeClass("speedtest-slow").addClass("speedtest-fast");
+            console.log("speedtest force", speedtest.name, speedtest.kbps);
+            return;
+        }
+
 
         Adapt.config.set('_canLoadData', false);
 
